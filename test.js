9a3ef8b9ebcf8eaac326b90f4a7aac9f5bce45d8
Joshua Moyers <jmoyers@gmail.com>
2011-07-31 23:52:04 -0700

    epoch crappy doc parser

---
+++ test.js
@@ -0,0 +1,17 @@
+/**
+ * Song
+ *
+ * @author Joshua Moyers <jmoyers@gmail.com>
+ * @copyright Joshua Moyers - July 31, 2011
+ * @package newco
+ */
+
+var util = require('util'),
+    inherits = util.inherits;
+
+/**
+ * A test method for kicking total ass
+ *
+ * @return Number
+ */
+Class.prototype.methodName = function(){}
